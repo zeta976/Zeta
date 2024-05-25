85d4c32a77110e17bc4cc4fd64814b5619c981ec
unknown <alejandrozuluagarincon2007@gmail.com>
2024-05-25 14:44:16 -0500

    Add footer to index.html

--- app.js
+++ app.js
@@ -58,3 +58,13 @@ faqs.forEach((faq) => {
         }
     });
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
